9e7e1888564794c87779bbfeee8826c1aef0622c
Nomad29 <alexandramelancon@outlook.com>
2020-03-07 04:55:05 -0500

    Roads + intersections
    Added the road and intersections, all with working colliders considering the gravity.

---
+++ prototype2/Assets/BlueGreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueGreen : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            // Write here what happens when the player goes on the intersection between the blue and green roads
+        }
+    }
+}
---
+++ prototype2/Assets/RoadBlue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadBlue : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            // Write here what happens when the player is on the blue road
+            Debug.Log("On Blue");
+        }
+    }
+}
